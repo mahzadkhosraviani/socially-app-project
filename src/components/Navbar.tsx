ff31c30c54a8716df0c41c4585796c14794fc5b1
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-02-20 11:34:31 +0330

    adding more ui such at navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,52 @@
+// import { useEffect, useState } from "react";
+
 function Navbar() {
+  // const [dark, setDark] = useState(() => {
+  //   const saved = localStorage.getItem("theme");
+  //   return saved === "dark";
+  // });
+  // useEffect(() => {
+  //   if (dark) {
+  //     document.body.className = "bg-black text-white";
+  //     localStorage.setItem("theme", "dark");
+  //   } else {
+  //     document.body.className = "bg-white text-black";
+  //     localStorage.setItem("theme", "light");
+  //   }
+  // }, [dark]);
   return (
     <>
-      <div className="flex justify-between items-center my-5 mx-6">
-        <span className="text-2xl font-bold">socially</span>
-        <div className="flex gap-5">
-          <button className="py-1 px-2 md:py-2 md:px-4 rounded-md text-md md:text-xl shadow-sm">
+      <div className="flex justify-between items-center my-3 mx-7 md:mx-40">
+        <span className="text-xl md:text-2xl tracking-wide font-semibold font-mono">
+          socially
+        </span>
+        <div className="flex gap-3 md:gap-7">
+          <button
+            // onClick={() => setDark(!dark)}
+            className="cursor-pointer py-1 px-2 md:py-2 md:px-4 rounded-md text-md md:text-xl shadow-lg"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              className="bi bi-brightness-high"
+              className="bi bi-brightness-high md:size-5"
               viewBox="0 0 16 16"
             >
               <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
             </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="hidden bi bi-moon" viewBox="0 0 16 16">
+  <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286"/>
+</svg>
           </button>
 
-          <button className="flex gap-1 justify-center items-center ">
+          <button className="text-md md:text-lg cursor-pointer flex gap-1 justify-center items-center ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              className="bi bi-house-door"
+              className="bi bi-house-door md:size-5"
               viewBox="0 0 16 16"
             >
               <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z" />
@@ -31,11 +54,12 @@ function Navbar() {
             Home
           </button>
 
-          <button className="bg-black text-white py-1 px-2 md:py-2 md:px-4 rounded-md text-md md:text-xl">
+          <button className="cursor-pointer bg-black text-white py-1 px-2 md:py-2 md:px-4 rounded-md text-md md:text-lg">
             Sign In
           </button>
         </div>
-      </div>
+      </div>{" "}
+      <hr className="text-[#e5e5e5]" />
     </>
   );
 }
