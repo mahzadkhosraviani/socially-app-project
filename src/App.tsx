667d836f5c2ed251d912bf1abb0cbd384ea6c166
parham <parhamesfahani2004bar@gmail.com>
2026-02-22 15:34:32 +0330

    add profile feature1

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+// import Profile from "./components/profile";
 function App() {
 
 
   return (
-    <>
-     <Routes>
-      <Route path="/" element={<SignIn />} />
+     <>
+    <Routes>
+     <Route path="/" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
     </Routes>
     </>
+
+     
+   // <Routes>
+     // <Route path="/" element={<Profile />} />
+    // </Routes>
+   
   )
 }
 
