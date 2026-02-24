b41424671092437fd58a9702119e1d42f4de0b83
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-02-24 10:42:27 +0330

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SignUp from "./components/SignUp";
 import CreatePost from "./components/createPost";
 import PostCard from "./components/postCard";
 import RecommendedUsers from "./components/recommendedusers";
+import { useState } from "react";
 // import Profile from "./components/profile";
 
 // <Routes>
