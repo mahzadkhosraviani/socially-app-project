b0dfec90a68a66fe433e5dd84055324a3d7c8985
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-02-28 01:27:31 +0200

    initial changes

--- src/components/MainProfilePosts.tsx
+++ src/components/MainProfilePosts.tsx
@@ -1,8 +1,11 @@
 const MainProfilePosts = () => {
   return (
     <>
-      <div className=""><p className="">There is no post.</p>
-      <p className="">this user not posted anything.</p></div>
+      <div className="">
+        <p className="">There is no post.</p>
+        <p className="">this user not posted anything.</p>
+      </div>
     </>
   );
 };
+export default MainProfilePosts;
