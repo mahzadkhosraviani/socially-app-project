1cb619ad0460d1cd30147a2436b405ea131daa9e
Nastaran <nastaranadib1383@gmail.com>
2026-04-30 13:48:20 +0330

    clicking on the recommended user to display their information

--- src/components/ProfileContainer.tsx
+++ src/components/ProfileContainer.tsx
@@ -2,6 +2,8 @@ import PorfileCard from "./PorfileCard";
 import PorfileStats from "./PorfileState";
 import EditButton from "./EditButton";
 import UserInfo from "./UserInfo";
+import { useEffect, useState } from "react";
+import { authService } from "../services/authService";
 
 interface ProfileContainerProps {
   name: string;
@@ -15,21 +17,38 @@ interface ProfileContainerProps {
   createdAt: string;
 }
 
-const ProfileContainer = ({user}) => {
+const ProfileContainer = ({ user }) => {
+  const [userInfoNew, setUserInfoNew] = useState(null);
+  const following = user?._count?.followings ?? userInfoNew?._count?.followings;
+  const followers = user?._count?.followers ?? userInfoNew?._count?.followers;
+  const [mainUser, setMainUser] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await authService.getUserById(user.id);
+        setUserInfoNew(res.data.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    if (!following && !followers) {
+      setMainUser(true);
+      fetchData();
+    } else {
+      setMainUser(false);
+    }
+  }, [user?.id]);
+  
   return (
-
     <div className="w-[550px] mx-auto h-110 p-4 bg-white dark:bg-black dark:border-[#262626] dark:border rounded-2xl shadow flex flex-col gap-4 mb-7">
-      <PorfileCard user = {user}/>
+      <PorfileCard user={user} />
 
+      <PorfileStats followings={following} followers={followers} posts={3} />
+      {!mainUser && <EditButton label="Follow" />}
+      {mainUser && <EditButton label="Edit Profile" />}
 
-      <PorfileStats
-        followings={1}
-        followers={2}
-        posts={3}
-      />
-      <EditButton label="Edit Profile" />
-
-      <UserInfo user = {user} />
+      <UserInfo user={user} />
     </div>
   );
 };
