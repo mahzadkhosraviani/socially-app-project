310b5b9b5ce14fad2ad52f2da4e847fcbbe740ef
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-05-04 12:43:40 +0330

    adding scroll bar on dashbord profile

--- src/components/layoutProfile.tsx
+++ src/components/layoutProfile.tsx
@@ -1,18 +1,45 @@
+// import Navbar from "./Navbar";
+// import Profile from "./profile";
+
+// const LayoutProfile = ({ children }) => {
+//   return (
+//     <div className=" bg-white dark:bg-[#0A0A0A] flex flex-col">
+//       <Navbar />
+
+//       <div className="flex flex-row  gap-6 mt-7 ">
+//         <div className="hidden md:block ">
+//           <Profile />
+//         </div>
+//         <div className="flex flex-1 flex-col  md:mx-auto w-full mx-5 mb-3  ">
+//           {children}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LayoutProfile;
 import Navbar from "./Navbar";
 import Profile from "./profile";
 
 const LayoutProfile = ({ children }) => {
   return (
-    <div className=" bg-white dark:bg-[#0A0A0A] flex flex-col">
+    <div className="h-screen flex flex-col bg-white dark:bg-[#0A0A0A] ">
       <Navbar />
 
-      <div className="flex flex-row  gap-6 mt-7 ">
-        <div className="hidden md:block ">
-          <Profile />
-        </div>
-        <div className="flex flex-1 flex-col  md:mx-auto w-full mx-5 mb-3  ">
-          {children}
-        </div>
+      <div className="flex flex-1 overflow-hidden flex-row gap-6 ">
+        <aside className="hidden md:block ">
+          <div className="sticky md:pt-8  overflow-hidden ">
+            <Profile />
+          </div>
+        </aside>
+       
+     
+        <main className="flex-1 overflow-y-auto">
+          <div className="flex flex-1 flex-col md:pt-8 md:mx-auto  md:w-full mx-2 mb-3  ">
+            {children}
+          </div>
+        </main>
       </div>
     </div>
   );
