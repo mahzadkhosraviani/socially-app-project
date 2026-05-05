04b654e0838fe0fbb993035ad975e31f8a892eff
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-05-05 21:09:42 +0330

    adding changes

--- src/pages/DashboardNotification.tsx
+++ src/pages/DashboardNotification.tsx
@@ -8,7 +8,7 @@ function DashboardNotification() {
     <div className="bg-white dark:bg-[#0A0A0A]">
       <Navbar />
       <div className="min-h-screen mt-6 md:mt-8 flex flex-row gap-4 mx-5 dark:bg-[#0A0A0A]">
-        <div className="hidden md:block  "><Profile/></div>
+        <div className="hidden md:block "><Profile/></div>
         <NotificationBox/>
       </div>
     </div>
