06c08f3249fb37c420039d9283dc7396cd576944
peyman <amirmahani.ce@gmail.com>
2026-05-10 18:39:56 +0330

    fixed the posts deletation and toggle like in profile section not working correctly

--- src/hooks/use-toggleLike.ts
+++ src/hooks/use-toggleLike.ts
@@ -5,11 +5,10 @@ export const useToggleLike = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async (postId: string) => {
-      await postService.likePost(postId);
-    },
+    mutationFn: (postId: string) => postService.likePost(postId),
     onSuccess: (_, postId) => {
       queryClient.invalidateQueries({ queryKey: ["posts"] });
+      queryClient.invalidateQueries({ queryKey: ["profileContent"] });
       queryClient.invalidateQueries({ queryKey: ["post", postId] });
     },
   });
